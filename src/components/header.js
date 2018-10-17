c99ad2473c46f7e31df13cdde67b54b664aae5ea
georgeosemwegie <g4real2006@gmail.com>
2018-10-17 14:17:29 -0600

    completed project

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="header">
       <div className="header__skew" />
       <div className="header__border" />
-      <h1>Bottega Madlibs</h1>
+      <h1>George Madlibs</h1>
       <p>
         Fill out the fields below and click the generate button to see the Mad
         Lib story.
