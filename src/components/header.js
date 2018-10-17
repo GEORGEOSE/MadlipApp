963e120f5a27800ea486ec90e6ee785a6e080d0d
georgeosemwegie <g4real2006@gmail.com>
2018-10-17 12:23:29 -0600

    added header h1 and p styles

--- src/components/header.js
+++ src/components/header.js
@@ -7,8 +7,8 @@ const Header = () => {
       <div className="header__border" />
       <h1>Bottega Madlibs</h1>
       <p>
-        Fill out the fields below and click the generate button to see the
-        MadLib story.
+        Fill out the fields below and click the generate button to see the Mad
+        Lib story.
       </p>
     </div>
   );
